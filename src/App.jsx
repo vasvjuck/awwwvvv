e5606fe8502fb0b56dbcf9305be1ca72dbc96242
Vasyl Vasiuk <vasyl.vasiuk@moonshiner.at>
2024-11-24 21:30:04 +0100

    feat: initialize "awwwvvv"

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidde">
+      <Navbar />
+      <Hero />
+      <About />
+    </main>
+  )
+}
+
+export default App
